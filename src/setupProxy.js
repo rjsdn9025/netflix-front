8912a01dc311ccf76c22cac2d169ccae6ae9c101
rjsdn9025 <rjsdn9025@naver.com>
2024-09-19 18:09:52 +0900

    change code

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api', // /api로 시작하는 요청을 프록시
+    createProxyMiddleware({
+      target: 'http://internal-back-alb-1882035709.ap-northeast-2.elb.amazonaws.com:5000', // 백엔드 서버 주소
+      changeOrigin: true,
+    })
+  );
+};
